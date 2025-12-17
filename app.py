8a44d0e2e9a84f110eba04d1d6a81fb9207ad7d1
archana <archanabegari840@gmail.com>
2025-12-17 12:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pathlib import path 
+from pathlib import Path 
 st.set_page_config (
     page_title="Student Task Dashboard",
     layout="wide"
@@ -7,3 +7,4 @@ st.set_page_config (
 )
 html_file=Path("index.html").read_text(encoding="utf-8")
 st.components.v1.html(html_file,height=900,scrolling=True)
+
